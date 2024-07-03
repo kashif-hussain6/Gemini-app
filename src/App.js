e01d32bfc4e9042c2f3e32aa63fe498682031bf5
kashif-aly6466 <159264621+kashif-aly6466@users.noreply.github.com>
2024-07-03 02:36:09 -0700

    make a searchBar

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import Main from "./compnent/Main/Main";
 import Sidebar from "./compnent/Sidebar/Sidebar";
 
 function App() {
+  
   return (
     <div className="body bg-white">
       <div className="flex ">
         <Sidebar />
         <Main />
+        
         
       </div>
     </div>
--- src/compnent/Card/Card.jsx
+++ src/compnent/Card/Card.jsx
@@ -1,12 +1,18 @@
-import React from 'react';
-
+import React from "react";
 const Card = ({ text, icon }) => {
   return (
-    <div className="bg-gray-100 p-4 rounded-lg shadow-lg flex flex-col items-center">
-      <p className="text-lg text-center">{text}</p>
-      <img src={icon} alt="icon" className="w-6 h-6 mt-4" />
+    <div className="relative p-4 flex flex-col  justify-between bg-[#e6eaf1] hover:bg-gray-300 duration-100 rounded-2xl h-[200px] w-[200px]">
+      <div className="flex-grow">
+        <p className="text-lg  w-[150px] mb-2 text-black opacity-80">{text}</p>
+       
+      </div>
+      <div className="absolute bottom-4 right-4 bg-white rounded-full p-2 ">
+        <img src={icon} alt="Icon" className="w-6 h-6 text-gray-500  " />
+       
+
+      </div>
     </div>
   );
-}
+};
 
 export default Card;
